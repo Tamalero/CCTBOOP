7d49ef3bdd337f4404df4dc5cb8d48c6f4508a1f
Cesar R <tamalero@gmail.com>
2024-02-12 12:12:13 -0600

    Update Main.java
    Added the CLASS section.
    
    Note that this is currently using the "test" table on the remote (mikaela's planetscale's server).
    
    Also assuming that it will only have 3 fields. ID, Classname and teacherID.

--- src/main/java/com/school/projectschool/Main.java
+++ src/main/java/com/school/projectschool/Main.java
@@ -152,35 +152,178 @@ public class Main {
                     break;
                 }
                 case 'C': {
-                    System.out.println("Selecting Class Management ");
+                    System.out.println("\n Selecting Class Management \n");
                     do {
-                        System.out.println("Please select from the options: ");
+                        System.out.println("\nPlease select from the options: \n");
                         System.out.println("For inserting new Class data, select N ");
                         System.out.println("For Editing Class data, select E: ");
                         System.out.println("For Deleting Class data, select D: ");
-                        System.out.println("To return to the prior menu press X ");
+                        System.out.println("To return to the prior menu press X \n");
                         classMenu = menuOption.next().charAt(0);
 
                         switch (classMenu) {
                             case 'N': {
                                 System.out.println("Insert Class mode enabled: ");
-                                // call external class for connecting to DB
-                                break;
-                            }
+
+                                try (Connection connection = MySqlConnection.getConnection()) {
+                                    // performsn first select from to showcase what the user is seeing.
+                                    String query = "SELECT * FROM test";
+                                    System.out.println("Class DB contains: ");
+                                    try (PreparedStatement preparedStatement = connection.prepareStatement(query);
+                                         ResultSet resultSet = preparedStatement.executeQuery()) {
+                                        while (resultSet.next()) {
+                                            System.out.println(resultSet.getString((1)) + ", " + resultSet.getString(2)+ ", " + resultSet.getString(3));
+
+                                        }
+                                    }
+                                } catch (SQLException e) {
+                                    throw new RuntimeException(e); }
+
+                                System.out.println("\n Please insert the new Class' name: ");
+                                String className = scan.nextLine();
+                                System.out.println("Assign the Teacher ID now: ");
+                                int teacherID = Integer.parseInt(scan.nextLine());
+                                //String query = "INSERT INTO test (Classname,TeacherId) values ('" + className + "',"+ teacherID +");";
+
+                                try
+                                        (Connection connection = MySqlConnection.getConnection()) {
+                                    // Does the INSERT function
+                                    String query = "INSERT INTO test (Classname,TeacherId) values ('" + className + "',"+ teacherID +");";
+                                    System.out.println(query);
+                                       try (PreparedStatement conn = connection.prepareStatement(query)) {
+                                           int return1 = conn.executeUpdate();
+                                           System.out.println("Updated Rows: " + return1); // returns the number of rows affected
+                                           }
+                                     query = "SELECT * FROM test"; // shows the resulted table
+                                       System.out.println("\nUpdated Table \n");
+                                    try (PreparedStatement conn = connection.prepareStatement(query);
+                                        ResultSet resultSet = conn.executeQuery()) {
+                                            while (resultSet.next()) {
+                                                System.out.println(resultSet.getString((1)) + ", " + resultSet.getString(2)+ ", " + resultSet.getString(3));
+                                            }
+                                    }
+
+
+
+                                } catch (SQLException e) {
+                                    throw new RuntimeException(e); }
+
+
+                                    break;
+                                }
+
                             case 'E': {
                                 System.out.println("Editing Class mode enabled: ");
                                 // call external class for connecting to DB
+
+                                try (Connection connection = MySqlConnection.getConnection()) {
+                                    // performsn first select from to showcase what the user is seeing.
+                                    String query = "SELECT * FROM test";
+                                    System.out.println("Class DB contains: ");
+                                    try (PreparedStatement preparedStatement = connection.prepareStatement(query);
+                                         ResultSet resultSet = preparedStatement.executeQuery()) {
+                                        while (resultSet.next()) {
+                                            System.out.println(resultSet.getString((1)) + ", " + resultSet.getString(2)+ ", " + resultSet.getString(3));
+
+                                        }
+                                    }
+                                } catch (SQLException e) {
+                                    throw new RuntimeException(e); }
+
+                                System.out.println("\n Please insert the ID of the class row to edit: ");
+                                int classID = Integer.parseInt(scan.nextLine());
+                                System.out.println("Enter the replacement name for this class: ");
+                                String className = scan.nextLine();
+                                System.out.println("Assign the Teacher ID now: ");
+                                int teacherID = Integer.parseInt(scan.nextLine());
+                                //String query = "UPDATE test set Classname = " +className+ ", TeacherId " + teacherID + " WHERE id = " + classID +";";
+
+                                try
+                                        (Connection connection = MySqlConnection.getConnection()) {
+                                    // Does the UPDATE function
+                                    String query = "UPDATE test SET Classname = '" +className+ "', TeacherId = " + teacherID + " WHERE id = " + classID +";";
+                                    System.out.println(query);
+                                    try (PreparedStatement conn = connection.prepareStatement(query)) {
+                                        int return1 = conn.executeUpdate();
+                                        System.out.println("Updated Rows: " + return1); // returns the number of rows affected
+                                    }
+                                    query = "SELECT * FROM test"; // shows the resulted table
+                                    System.out.println("\nUpdated Table \n");
+                                    try (PreparedStatement conn = connection.prepareStatement(query);
+                                         ResultSet resultSet = conn.executeQuery()) {
+                                        while (resultSet.next()) {
+                                            System.out.println(resultSet.getString((1)) + ", " + resultSet.getString(2)+ ", " + resultSet.getString(3));
+                                        }
+                                    }
+
+
+
+                                } catch (SQLException e) {
+                                    throw new RuntimeException(e); }
+
 
                                 break;
                             }
                             case 'D': {
                                 System.out.println("Deleting Class mode enabled: ");
                                 // call external class for connecting to DB
+                                try (Connection connection = MySqlConnection.getConnection()) {
+                                    // performsn first select from to showcase what the user is seeing.
+                                    String query = "SELECT * FROM test";
+                                    System.out.println("Class DB contains: ");
+                                    try (PreparedStatement preparedStatement = connection.prepareStatement(query);
+                                         ResultSet resultSet = preparedStatement.executeQuery()) {
+                                        while (resultSet.next()) {
+                                            System.out.println(resultSet.getString((1)) + ", " + resultSet.getString(2)+ ", " + resultSet.getString(3));
 
-                                break;
-                            }
+                                        }
+                                    }
+                                } catch (SQLException e) {
+                                    throw new RuntimeException(e); }
+
+                                System.out.println("\n Please insert the ID of the class row to DELETE: ");
+                                int classID = Integer.parseInt(scan.nextLine());
+                                //String query = "DELETE FROM test WHERE id = " + classID +";";
+
+                                String query = "DELETE FROM test WHERE id = " + (classID) + ";";
+                                System.out.println(query + "\n");
+                                System.out.println("Are you sure you want to delete this class? (Yes/No)\n");
+                                String AreYouSure = scan.nextLine();
+                                if (AreYouSure.equals("Yes")) {
+                                    try (Connection connection = MySqlConnection.getConnection()) {
+                                        // Does the DELETE function
+                                        try (PreparedStatement conn = connection.prepareStatement(query)) {
+                                            int return1 = conn.executeUpdate();
+                                            System.out.println("Updated Rows: " + return1); // returns the number of rows affected
+
+                                        }
+                                    } catch (SQLException e) {
+                                        throw new RuntimeException(e);
+                                    }
+
+                                    query = "SELECT * FROM test"; // shows the resulted table
+                                    System.out.println("\nUpdated Table \n");
+                                    try (Connection connection = MySqlConnection.getConnection()) {
+                                        try (PreparedStatement conn = connection.prepareStatement(query);
+                                             ResultSet resultSet = conn.executeQuery()) {
+                                            while (resultSet.next()) {
+                                                System.out.println(resultSet.getString((1)) + ", " + resultSet.getString(2) + ", " + resultSet.getString(3));
+                                            }
+
+
+                                        } catch (SQLException e) {
+                                            throw new RuntimeException(e);
+                                        }
+                                    } catch (SQLException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                }else { }
+
+
+                                break; }
+
                             case 'X': {
-                                System.out.println("Returning to main menu. ");
+                                System.out.println("Returning to main menu. \n");
                                 classMenu = 'X';
                                 break;
                             }
